54a965bdecde4227d414b3ee2e03934a6603b01f
lewis mcmillan <lewistmcmillan@gmail.com>
2020-12-09 12:37:03 +0000

    Refactored code. Moved Fresnel and intersection routines to separate module.

--- src/constants.f90
+++ src/constants.f90
@@ -2,6 +2,6 @@ module constants
 
     implicit none
 
-    real, parameter :: pi = 4.*atan(1.), twopi=2.*4.*atan(1.) 
+    real, parameter :: pi = 4.*atan(1.), twopi=2.*4.*atan(1.)
     
 end module constants
