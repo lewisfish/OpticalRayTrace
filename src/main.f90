f7c10db11ba96ad5d982d86032c3c95e90b3da01
lewis mcmillan <lewistmcmillan@gmail.com>
2021-03-05 11:50:41 +0000

    Added new features to simulation that were missed in previous commits.

--- src/main.f90
+++ src/main.f90
@@ -1,14 +1,14 @@
 program raytrace
 
     use lensMod,      only : plano_convex, achromatic_doublet, glass_bottle
-    use source,       only : ring, point, emit_image, create_spot, init_emit_image
+    use source,       only : ring, point, emit_image, create_spot, init_emit_image, cross, ring2
     use stackMod,     only : stack
     use utils,        only : str, pbar
     use vector_class, only : vector
     use setup
     use imageMod
     use OMP_LIB
-    use random, only : ran2
+    use random, only : ran2, init_rng
     use iso_fortran_env, only: int64
 
     implicit none
@@ -26,10 +26,11 @@ program raytrace
     integer(int64) :: rcount, pcount
     real           :: d, angle, cosThetaMax, r1, r2
     real           :: besselDiameter, distance
-    logical        :: skip
+    logical        :: skip, file_exists
     integer        :: imgin(512,512), nphotonsLocal
     character(len=:), allocatable :: filename
 
+    file_exists = .false.
     image = 0
     rcount = 0_int64! counter for how many photons dont make from the ring source
     pcount = 0_int64! counter for how many photons dont make from the point source
@@ -38,7 +39,8 @@ program raytrace
 
     filename = trim(adjustl(source_type))//"_bottle_"//str(use_bottle)//"_Ra_"// &
                     str(bottle%radiusa,7)//"_Rb_"//str(bottle%radiusb,7)//"_offset_"//&
-                    str(bottle%centre%z,7)//"_"//str(iris)//"_"//str(iris_radius, 3)//"_L2f_"//str(L2%f,6)//"_L3f_"//str(L3%f,6)
+                    str(bottle%centre%z,7)//"_"//str(iris)//"_"//str(iris_radius, 7)//"_L2f_"//str(L2%f,6)//"_L3f_"//str(L3%f,6)//&
+                    "_fo_"//str(fibre_offset, 7)
 
     !max angle for point source to lens
     angle = atan(L2%radius / L2%fb)
@@ -52,10 +54,11 @@ program raytrace
     ! distance to surface of bottle
     distance = L2%fb - (bottle%radiusa - bottle%centre%z)
     !https://en.wikipedia.org/wiki/Axicon#/media/File:Erzeugen_von_Besselstrahlen_durch_ein_Axicon.png
-    besselDiameter = 2.* distance * tan(alpha*(n-1.))
+    ! besselDiameter = 2.* distance * tan(alpha*(n-1.)) !old
 
+    besselDiameter = distance*97.3d-3*tan(alpha* (n - 1)) /(l2%fb)
     !annulus radii, squared as this is required for sampling
-    r1 = besselDiameter/1.0d0 - ringWidth
+    r1 = besselDiameter/2.0d0 - ringWidth
     r2 = (besselDiameter / 2.d0)**2
     r1 = r1**2
 
@@ -65,10 +68,12 @@ program raytrace
     
     bar = pbar(nphotons/ 1000000)
 
+    !make repeatable
+    call init_rng(123456789)
 !$OMP parallel default(none)&
 !$OMP& shared(L2, L3, bottle, uring, upoint, nphotons, cosThetaMax, r1, r2, image, wavelength)&
 !$OMP& shared(use_tracker, use_bottle, image_source, point_source, spot_source, filename, folder)&
-!$OMP& shared(source_type, bar, iris, iris_radius)&
+!$OMP& shared(source_type, bar, iris, iris_radius, image_diameter, fibre_offset, L2file, L3file)&
 !$omp& private(d, pos, dir, skip, tracker), firstprivate(imgin), reduction(+:rcount, pcount)
 !$OMP do
     do i = 1, nphotons
@@ -100,21 +105,21 @@ program raytrace
         end if
 
         !move to image plane
-        d = ((2.*(L2%fb + L3%fb) + L2%thickness + L3%thickness) - pos%z) / dir%z
+        d = ((2.*(L2%fb + L3%fb) + L2%thickness + L3%thickness + fibre_offset) - pos%z) / dir%z
         ! d = (L2%thickness+L2%fb +L2%fb+L3%fb+  L3%thickness+L3%fb- pos%z) / dir%z
         pos = pos + dir * d
         if(use_tracker)call tracker%push(pos)
-        call makeImage(image, pos, 1d-2, 1)
+        call makeImage(image, dir, pos, image_diameter, 1)
         if(use_tracker)call tracker%write(uring)
     end do
 !$OMP end do
 
 if(use_tracker)close(uring)
-! $OMP single
-    ! wavelength = 843d-9
-    ! L2 = plano_convex("../res/planoConvex.params", wavelength)
-    ! L3 = achromatic_doublet("../res/achromaticDoublet.params", wavelength, L2%fb)
-! $OMP end single
+!$OMP single
+    wavelength = 843d-9
+    L2 = plano_convex("../res/"//trim(L2file), wavelength)
+    L3 = achromatic_doublet("../res/"//trim(L3file), wavelength, L2%fb, L2%thickness)
+!$OMP end single
     !max angle for point source to lens
     ! angle = (13.*pi)/90.
     ! cosThetaMax = cos(angle)
@@ -134,6 +139,7 @@ if(use_tracker)close(uring)
         if(image_source)then
             call emit_image(imgin, pos, dir, L2)
         elseif(point_source)then
+            ! call cross(pos, dir)
             call point(pos, dir, cosThetaMax)
         elseif(spot_source)then
             call create_spot(pos, dir, cosThetaMax, nphotons, i)
@@ -164,11 +170,11 @@ if(use_tracker)close(uring)
         end if
 
         !move to image plane
-        d = ((2.*(L2%fb + L3%fb) + L2%thickness + L3%thickness) - pos%z) / dir%z
+        d = ((2.*(L2%fb + L3%fb) + L2%thickness + L3%thickness + fibre_offset) - pos%z) / dir%z
 
         pos = pos + dir * d
         if(use_tracker)call tracker%push(pos)
-        call makeImage(image, pos, 1d-2, 2)
+        call makeImage(image, dir, pos, image_diameter, 2)
         if(use_tracker)call tracker%write(upoint)
     end do
 !$OMP end do
@@ -176,12 +182,15 @@ if(use_tracker)close(uring)
 
 if(use_tracker)close(upoint)
 
-open(newunit=upoint, file=folder//"trans-stats.dat", position="append")
+inquire(file=folder//"trans-stats.dat", exist=file_exists)
+if(.not. file_exists)then
+    open(newunit=upoint, file=folder//"trans-stats.dat")
+    write(upoint, *)"r/%, p/%, l2%f, l3%f, bottle?, radiusA, radiusB, iris_pos, iris_radius, offset"
+else
+    open(newunit=upoint, file=folder//"trans-stats.dat", position="append")
+end if
 write(upoint,*)100.*(1.-(rcount/(real(nphotons)))),",", 100.*(1.-(pcount/(real(nphotons)))),",", L2%f,",",&
-          L3%f,",", use_bottle,",", bottle%radiusa,",", bottle%radiusb
-
-! write(upoint,*)100.*(1.-(rcount/(real(nphotons)))),",", 100.*(1.-(pcount/(real(nphotons)))),",", iris,",",&
-          ! iris_radius,",", use_bottle,",", bottle%radiusa,",", bottle%radiusb
+          L3%f,",", use_bottle,",", bottle%radiusa,",", bottle%radiusb,",", iris,",", str(iris_radius,7), ",", bottle%centre%z
 close(upoint)
 
 print"(A,1X,f8.2,A)","Ring  transmitted: ",100.*(1.-(rcount/(real(nphotons)))),"%"
